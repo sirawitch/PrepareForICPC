7d7d41c727821f526eb8a1c20d952369fcefac5e
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2023-11-01 18:56:46 +0700

    J pass

--- ICPC/2022_Manila_J.cpp
+++ ICPC/2022_Manila_J.cpp
@@ -9,50 +9,6 @@ int rnd=0;
 int distance(pair<int,int> a,pair<int,int>b){
     return abs(a.first-b.first)+abs(a.second-b.second);
 }
-void create_path_1(pair<int,int> u, pair<int,int>v){
-    if(u.first==v.first&&u.second==v.second)return;
-    if(u.first<v.first && path[u.first+1][u.second]==rnd){
-        path[u.first + 1][u.second]=rnd+1;
-        create_path_1(make_pair(u.first + 1, u.second), v);
-    }
-    else if (u.first > v.first && path[u.first - 1][u.second] == rnd)
-    {
-        path[u.first - 1][u.second]=rnd+1;
-        create_path_1(make_pair(u.first - 1, u.second), v);
-    }
-    if (u.second < v.second && path[u.first][u.second+1] == rnd)
-    {
-        path[u.first][u.second+1]=rnd+1;
-        create_path_1(make_pair(u.first, u.second+1), v);
-    }
-    else if (u.second > v.second && path[u.first][u.second-1] == rnd)
-    {
-        path[u.first][u.second-1]=rnd+1;
-        create_path_1(make_pair(u.first, u.second-1), v);
-    }
-}
-void createSol(pair<int,int>u,pair<int,int>v){
-    if(u.first==v.first&&u.second==v.second)return;
-    if(u.first>v.first){
-        s[u.first-1][u.second]='#';
-        createSol(make_pair(u.first-1,u.second),v);
-    }
-    else if (u.first < v.first)
-    {
-        s[u.first + 1][u.second] = '#';
-        createSol(make_pair(u.first + 1, u.second), v);
-    }
-    else if (u.second > v.second)
-    {
-        s[u.first][u.second-1] = '#';
-        createSol(make_pair(u.first, u.second-1), v);
-    }
-    else if (u.second < v.second)
-    {
-        s[u.first][u.second+1] = '#';
-        createSol(make_pair(u.first, u.second+1), v);
-    }
-}
 int main(){
     pair<int,int> p[3];
     scanf("%d%d",&r,&c);
@@ -65,52 +21,31 @@ int main(){
             if(s[i][j]=='#')p[k++]=make_pair(i,j);
         }
     }
-    int sumDis[3];
-    sumDis[0]=distance(p[0],p[1])+distance(p[0],p[2]);
-    sumDis[1] = distance(p[1], p[0]) + distance(p[1], p[2]);
-    sumDis[2] = distance(p[2], p[0]) + distance(p[2], p[1]);
-    pair<int,int> temp;
-    //Want p[0] is middle point for go to p[1] and p[2]
-    if(sumDis[0]<=sumDis[1]&&sumDis[0]<=sumDis[2]);//don't change because p[0] is min
-    else if(sumDis[1]<=sumDis[0]&&sumDis[1]<=sumDis[2]){//swap p[0],p[1]
-        temp=p[0];
-        p[0]=p[1];
-        p[1]=temp;
-    }
-    else
-    { // swap p[0],p[2]
-        temp = p[0];
-        p[0] = p[2];
-        p[2] = temp;
-    }
-    rnd=0;
-    create_path_1(p[0],p[1]);
-    rnd=1;
-    create_path_1(p[0], p[2]);
-    pair<int,int> bestforp1=p[0],bestforp2=p[0];
-    int bestdisp1=1e9,bestdisp2=1e9;
-    for (int i = 0; i < r; ++i)
+    pair<int,int> middle=p[2];
+    if ((p[0].second <= p[1].second && p[1].second <= p[2].second) || (p[2].second <= p[1].second && p[1].second <= p[0].second))
     {
-        for (int j = 0; j < c; ++j)
+        middle = p[1];
+    }
+    else if ((p[1].second <= p[0].second && p[0].second <= p[2].second) || (p[2].second <= p[0].second && p[0].second <= p[1].second))
+    {
+        middle = p[0];
+    }
+    //make line x
+    for(int i=p[0].first;i<=p[2].first;++i){
+        s[i][middle.second]='#';
+    }
+    //make line y
+    for(int i=0;i<3;++i){
+        for(int j=p[i].second;j<=middle.second;++j){
+            s[p[i].first][j]='#';
+        }
+        for (int j = p[i].second; j >= middle.second; --j)
         {
-            if(path[i][j]==2){
-                s[i][j]='#';
-                if(bestdisp1>distance(make_pair(i,j),p[1])){
-                    bestdisp1 = distance(make_pair(i, j), p[1]);
-                    bestforp1=make_pair(i,j);
-                }
-                if (bestdisp2 > distance(make_pair(i, j), p[2]))
-                {
-                    bestdisp2 = distance(make_pair(i, j), p[2]);
-                    bestforp2 = make_pair(i, j);
-                }
-            }
+            s[p[i].first][j] = '#';
         }
     }
-    createSol(p[1],bestforp1);
-    createSol(p[2],bestforp2);
     for (int i = 0; i < r; ++i)
     {
-            printf("%s\n",s[i]);
+        printf("%s\n",s[i]);
     }
 }
